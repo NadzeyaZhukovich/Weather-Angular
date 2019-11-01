3b24e174035bc0fc2e9caae130bc2212babbdd15
Nadzeya <naduva0505@gmail.com>
2019-11-01 11:28:50 +0100

    Added base project style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchCityComponent } from './search-city/search-city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCityComponent
   ],
   imports: [
     BrowserModule
