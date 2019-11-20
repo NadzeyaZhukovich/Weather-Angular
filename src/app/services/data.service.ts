0883f7d43039e05dac89e275dd8b114b006b3529
Nadzeya <naduva0505@gmail.com>
2019-11-20 22:53:42 +0100

    Renamed TemperatureUnits to temperatureUnits

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
-import {TemperatureUnits} from '../model/TemperatureUnits';
+import {TemperatureUnits} from '../model/temperatureUnits';
 
 @Injectable({
   providedIn: 'root'
