8a33e3ce107a96ef6044eab3005d09b523906754
Nadzeya <naduva0505@gmail.com>
2019-12-10 19:42:48 +0100

    Created components weather-item and weather-items-list; Added template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { DataService } from '../app/services/data.service';
 import { SearchComponent } from './search/search.component';
 import { TodayWeatherComponent } from './today-weather/today-weather.component';
+import { WeatherItemComponent } from './weather-item/weather-item.component';
+import { WeatherItemsListComponent } from './weather-items-list/weather-items-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    TodayWeatherComponent
+    TodayWeatherComponent,
+    WeatherItemComponent,
+    WeatherItemsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/weather-items-list/weather-items-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-items-list',
+  templateUrl: './weather-items-list.component.html',
+  styleUrls: ['./weather-items-list.component.scss']
+})
+export class WeatherItemsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
